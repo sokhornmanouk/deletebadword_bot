638e41b8dbb5c45332e7f040a8d64cb31ee95964
sokhornmanouk <sokhornmanouk@gmail.com>
2026-07-17 17:21:10 +0700

    index.js

--- telegram_badword_cloudflare/src/index.js
+++ telegram_badword_cloudflare/src/index.js
@@ -94,6 +94,29 @@ async function deleteBadMessage(update, env) {
   });
 }
 
+const untilDate = Math.floor(Date.now() / 1000) + 60; // 60 seconds
+
+await telegramApi(env, "restrictChatMember", {
+  chat_id: message.chat.id,
+  user_id: message.from.id,
+  permissions: {
+    can_send_messages: false,
+    can_send_audios: false,
+    can_send_documents: false,
+    can_send_photos: false,
+    can_send_videos: false,
+    can_send_video_notes: false,
+    can_send_voice_notes: false,
+    can_send_polls: false,
+    can_send_other_messages: false,
+    can_add_web_page_previews: false,
+    can_change_info: false,
+    can_invite_users: false,
+    can_pin_messages: false
+  },
+  until_date: untilDate
+});
+
 async function setWebhook(request, env) {
   const url = new URL(request.url);
   const suppliedKey = url.searchParams.get("key");
